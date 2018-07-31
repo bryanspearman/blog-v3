f5d650d0eccfb15d42ad9febdf6664277f61b70a
Bryan Spearman <bryanspearman@yahoo.com>
2018-07-31 09:36:31 -0400

    fixed author string functionality

--- models.js
+++ models.js
@@ -20,7 +20,7 @@ postSchema.methods.serialize = function() {
     id: this._id,
     title: this.title,
     content: this.content,
-    author: this.author
+    author: this.authorString
   };
 };
 
