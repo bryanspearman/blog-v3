7eb91b9abd79d16e51768fdf6b90871bb0f010d1
Bryan Spearman <bryanspearman@yahoo.com>
2018-07-31 13:49:42 -0400

    added in comment and author schemas

--- models.js
+++ models.js
@@ -2,14 +2,22 @@
 
 const mongoose = require("mongoose");
 
+const commentSchema = mongoose.Schema({ content: "string" });
+
 const postSchema = mongoose.Schema({
-  title: { type: String },
-  content: { type: String },
-  author: {
-    firstName: String,
-    lastName: String
-  },
-  date: Date
+  title: "string",
+  content: "string",
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "Author" },
+  comments: [commentSchema]
+});
+
+const authorSchema = mongoose.Schema({
+  firstName: "string",
+  lastName: "string",
+  userName: {
+    type: "string",
+    unique: true
+  }
 });
 
 postSchema.virtual("authorString").get(function() {
@@ -22,10 +30,11 @@ postSchema.methods.serialize = function() {
     title: this.title,
     content: this.content,
     author: this.authorString,
-    date: this.date
+    created: this.created
   };
 };
 
 const Post = mongoose.model("Post", postSchema);
+const Author = mongoose.model("Author", authorSchema);
 
 module.exports = { Post };
